1433706dfdb381f853127e7aafe24e2a47e51156
asoftbird <asoftbird@gmail.com>
2021-07-01 23:28:09 +0200

    initial commit

---
+++ softbirdbot.py
@@ -0,0 +1,15 @@
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = commands.Bot(command_prefix=';')
+
+@client.event
+async def on_ready():
+    print("yeehaw")
+
+
+client.run(TOKEN)
