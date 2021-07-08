e78a8a9c232512108c49c580b50f25db3786f9e3
asoftbird <asoftbird@gmail.com>
2021-07-09 00:19:26 +0200

    moved time utils to utils.py

--- utils/util.py
+++ utils/util.py
@@ -3,12 +3,17 @@ import time
 import datetime
 import re
 
+HOUR = 3600
+MINUTE = 60
+DAY = HOUR * 24
+MONTH = DAY * 30
+YEAR = DAY * 365
 MAX_ASYNCIO_SECONDS = 3456000 #~40 days
 
 """
 adapted from discord.utils.sleep_until
 
-original:
+original: (requires datetime module)
 async def sleep_until(when, result=None):
   if when.tzinfo is None:
         when = when.replace(tzinfo=datetime.timezone.utc)
@@ -19,7 +24,7 @@ async def sleep_until(when, result=None):
         delta -= MAX_ASYNCIO_SECONDS
     return await asyncio.sleep(max(delta, 0), result)"""
 
-async def sleep_until_unixtime(time_target, result=None):
+async def sleepUntilUnixtime(time_target, result=None):
     time_now = int(time.time()) #unix time in seconds, truncated to int
     delta = time_target - time_now
     while delta > MAX_ASYNCIO_SECONDS:
@@ -27,4 +32,25 @@ async def sleep_until_unixtime(time_target, result=None):
         delta -= MAX_ASYNCIO_SECONDS
     return await asyncio.sleep(max(delta, 0), result)
 
+def convertTimeStringToSeconds(int_time, unit):
+    # convert input time and unit to equivalent in seconds
+    int_time = int(int_time)
+    print(f"Got {int_time} time and unit {unit}")
+    print(f"Type int_time: {type(int_time)}, type unit: {type(unit)}")
+    if unit == "seconde" or unit == "seconden":
+        return int_time
+    elif unit == "minuut" or unit == "minuten":
+        print("match MINUUT")
+        return int_time * MINUTE
+    elif unit == "uur" or unit == "uren":
+        print("match UUR")
+        return int_time * HOUR
+    elif unit == "dag" or unit == "dagen":
+        return int_time * DAY
+    elif unit == "maand" or unit == "maanden":
+        return int_time * MONTH
+    elif unit == "jaar" or unit == "jaren":
+        return int_time * YEAR
+    else:
+        return 60
 
